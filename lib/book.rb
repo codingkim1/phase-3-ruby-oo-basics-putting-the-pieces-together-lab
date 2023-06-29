e0832c097e2924239f6bac66d1078c259cde54df
Alex <alexkimanthi92@gmail.com>
2023-06-30 01:58:04 +0300

    updated

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
 
+class Book
+    attr_accessor :title, :author,:page_count,:genre, :turn 
+    def initialize(title)
+        @title= title 
+        page_count= 272
+    end
+
+    def turn_page
+        @page_count += 1
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+ 
+
 
